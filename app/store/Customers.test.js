d169529e11dc307451dd8c7d5432a5bb02c5eb56
Kent C. Dodds <kent@doddsfamily.us>
2016-04-04 17:02:44 -0600

    Update where the tests are at

--- app/store/Customers.test.js
+++ app/store/Customers.test.js
@@ -1,3 +1,30 @@
 import test from 'ava'
+// import sinon from 'sinon' // you'll need to install this with `npm install --save-dev sinon`
+// import store from './Customers'
 
-test('empty test', t => t.pass())
+// change this from `test.todo(` to simply `test(`
+test.todo('customers should start with empty', t => {
+  // call store.getCustomers and verify the result is empty
+})
+
+// change this from `test.todo(` to simply `test(`
+test.todo('setting customers and getting them', t => {
+  // create two or more objects with a string property called `name`
+  // call store.setCustomers with an array of these objects
+  // call store.getCustomers
+  // validate that what is returned has the proper length
+  // validate that the contents are the same as the contents of the array you passed
+})
+
+// change this from `test.todo(` to simply `test(`
+test.todo('subscribing to the store', t => {
+  // create a function spy with `sinon.spy()`
+  // use that spy to subscribe to the store and assign the unsubscribe function
+  // call store.setCustomers
+  // validate that the spy was called once
+  // reset the spy with `spy.reset()`
+  // then call the unsubscribe function
+  // validate that calling store.setCustomers again will not call the spy
+})
+
+// add an afterEach here to reset the customers to an empty array
